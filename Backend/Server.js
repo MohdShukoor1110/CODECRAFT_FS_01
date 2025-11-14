11ef50bd27e6e1bc5f3cd80dca082a876edff683
MohdShukoor1110 <mohdshukoor2002@gmail.com>
2025-11-14 17:42:19 +0530

    Backend Part Done.

--- Backend/Server.js
+++ Backend/Server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+const authRoutes = require("./Routes/Auth");
 
 require("dotenv").config();
-
 const MONGOURI = process.env.MONGOURI;
 const PORT = process.env.PORT;
 
 const app = express();
 
-mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("MongoDB Connected")).catch(err => console.log(err));
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => console.log("MongoDB Connected.")).catch(err => console.log(err));
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
